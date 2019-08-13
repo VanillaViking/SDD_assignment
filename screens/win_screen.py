c87b29a7d9501a500e1fd6ce88058605181c5dc2
jx9hw <52519789+jx9hw@users.noreply.github.com>
2019-08-13 10:06:46 +1000

    Update win_screen.py

--- screens/win_screen.py
+++ screens/win_screen.py
@@ -21,9 +21,12 @@ def msg(gameDisplay, text, font, colour, pos):
     gameDisplay.blit(text_surface, (pos[0] - (text_surface.get_width()/2), pos[1] - (text_surface.get_height()/2))) #position minus half of text size in order to center it. 
 
 
-def win_loop:
+def win_loop: #displays winning text
     if p1_win = True:
-        #msg.
+        textSurf, textRect = text_objects(player1_name + " wins", text_large, white)
+        textRect.center = (DISPLAY.get_width()/2), (DISPLAY.get_height()/2)
+        gameDisplay.blit(textSurf, textRect)
     if p2_win = True:
-        #
-    
+        textSurf, textRect = text_objects(player2_name + " wins", text_large, white)
+        textRect.center = (DISPLAY.get_width()/2), (DISPLAY.get_height()/2)
+        gameDisplay.blit(textSurf, textRect)

e6be9e6f3f8e31cd84b331c940342272dc145537
Ashwin Rajesh <ashwinr2k2@gmail.com>
2019-08-29 22:57:39 +1000

    added animation to text input

--- interactables/text_input.py
+++ interactables/text_input.py
@@ -6,7 +6,7 @@ SFONT = pygame.font.SysFont('Arial', 20)
 
 class text_input():
     """text input class"""
-    def __init__(self, DISPLAY,x, y, w, h, text ='',text_col=(0,0,0)):
+    def __init__(self, DISPLAY,x, y, w, h, text ='',text_col=(0,0,0), anim=True):
         self.display = DISPLAY
         self.rect = pygame.Rect(x, y, w, h)
         self.active = False
@@ -14,7 +14,11 @@ class text_input():
         self.text = text
         self.text_surface = FONT.render(text,True, text_col)
         self.text_col = text_col
-    
+        self.anim = anim 
+        self.clicked = False
+        self.count = 0
+        self.anim_x = -10
+
     def activate(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
             if self.rect.collidepoint(event.pos):
@@ -37,6 +41,16 @@ class text_input():
                     self.text += event.unicode
                 self.text_surface = FONT.render(self.text, True,self.text_col)
 
-    def draw(self):
+    def draw(self): 
+            
         self.display.blit(self.text_surface, (self.rect.x+5,self.rect.y+5))
         pygame.draw.rect(self.display, (self.colour), self.rect, 2)
+        if self.active: 
+            if self.count <= 200 and self.count >= 100:
+                pygame.draw.rect(self.display, (self.count, self.count, self.count), pygame.Rect(self.rect.x+10+self.text_surface.get_width(), self.rect.y + 5, 3, self.text_surface.get_height() - 5)) 
+           
+            self.count = (-(1/25) * ((self.anim_x - 50)**2)) + 200
+
+        self.anim_x += 1
+        if self.anim_x >= 100:
+            self.anim_x = -40
